450b9afcafc86af8d49d13571e9add3a0f141d65
lgflare <liguofeng18@mails.ucas.ac.cn>
2023-03-07 14:16:08 +0800

    delete print sentences for debugging

--- user/xargs.c
+++ user/xargs.c
@@ -27,32 +27,23 @@ main(int argc, char *argv[]) {
     inst_args[i - 1] = argv[i];
   }
   i = argc - 1;
-  //fprintf(1, "i = %d\n", i);
-  //fprintf(1, "%s\n", inst_args[i - 1]);
   while ((read(0, &buf[j], 1)) > 0) {
     if (buf[j] == ' ') {
       buf[j] = 0;
       inst_args[i] = (char*)malloc(BUFSIZE);
       memmove(inst_args[i], buf, j);
-      //show_args(inst_args);
-      //fprintf(1, "%s\n", inst_args[i]);
       j = 0;
       i++;
       continue;
     } else if (buf[j] == '\n' || buf[j] == 0) {
       buf[j] = 0;
-      //fprintf(1, "buf: %s\n", buf);
       inst_args[i] = (char*)malloc(BUFSIZE);
       memmove(inst_args[i], buf, j);
-      //fprintf(1, "inst_args: %s\n", inst_args[0]);
-      //fprintf(1, "inst_args: %s\n", inst_args[1]);
-      //show_args(inst_args);
       pid = fork();
       if (pid < 0) {
         fprintf(2, "fork error\n");
         exit(1);
       } else if (pid == 0) {
-        //show_args(inst_args);
         exec(inst_args[0], inst_args);
         exit(0);
       } else {
@@ -67,7 +58,6 @@ main(int argc, char *argv[]) {
         continue;
       }
     }
-    //fprintf(1, "buf: %s\n", buf);
     j++;
   }
 
